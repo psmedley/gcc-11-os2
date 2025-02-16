fd9201bc8258f68e09e097b949ad6feda562b1d8
Paul Smedley <paul@smedley.id.au>
2025-02-16 18:44:26 +1030

    Update conf.cmd
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- conf.cmd
+++ conf.cmd
@@ -1,9 +1,9 @@
 set LDFLAGS=-Zomf -Zstack 8192 -Zhigh-mem
 set cc=gcc
-set path=u:\dev\gcc-os2-14.x\host-i386-pc-os2-emx\gcc;%path%
+set path=u:\dev\gcc-os2-15.x\host-i386-pc-os2-emx\gcc;%path%
 set LIBS= -lpthread
 set as=c:/usr/bin/as.exe
 set ld=c:/usr/bin/ld.exe
 set nm=c:/usr/bin/nm.exe
 set grep=grep.exe
-dash ./configure --disable-bootstrap --enable-languages=c,c++ --build=i686-pc-os2-emx --with-sysroot=/@unixroot --prefix=/@unixroot/usr/local1420 --mandir=/@unixroot/usr/local1420/share/man --infodir=/@unixroot/usr/local1420/share/info --enable-shared --enable-threads --enable-checking=release --disable-multilib --with-system-zlib --with-gcc-major-version-only --without-isl --with-tune=generic --with-arch=i686 --with-gnu-as --disable-libstdcxx-pch
+dash ./configure --disable-bootstrap --enable-languages=c,c++ --build=i686-pc-os2-emx --with-sysroot=/@unixroot --prefix=/@unixroot/usr/local1501 --mandir=/@unixroot/usr/local1501/share/man --infodir=/@unixroot/usr/local1501/share/info --enable-shared --enable-threads --enable-checking=release --disable-multilib --with-system-zlib --with-gcc-major-version-only --without-isl --with-tune=generic --with-arch=i686 --with-gnu-as --disable-libstdcxx-pch
